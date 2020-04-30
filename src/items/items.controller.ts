68d8674d6f45d824f26640c2e634e00935c43a4b
unknown <mikaeltruth@gmail.com>
2020-04-30 20:34:47 -0300

    api que lista url de servicos

---
+++ src/items/items.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Put, Param, Post, Body, Delete } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
+import { ItemsService } from './items.service';
+import { CreateItemDto } from './dto/create-item.dto';
+
+@Controller('item')
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  
+  @Get()
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+ findOne(@Param('id') id): Promise<Item> {
+  return this.itemsService.findOne(id);
+ }
+
+ @Post()
+ create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+  return this.itemsService.create(createItemDto);
+ }
+
+ @Post('many')
+ createMany(@Body() createItemDto: CreateItemDto[]): Promise<Item[]> {
+   return this.itemsService.createMany(createItemDto);
+ }
+ 
+ @Delete(':id')
+ delete(@Param('id') id): Promise<Item> {
+  return this.itemsService.delete(id);
+ }
+
+ @Put(':id')
+ update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+  return this.itemsService.update(id, updateItemDto);
+ }
+}
